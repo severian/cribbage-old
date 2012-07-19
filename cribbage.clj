695ac68ad00e2cecc3d9b4aa8b1b885d72829238
James Baird <severian@gmail.com>
2012-07-18 23:54:38 -0700

    More play scoring functions

--- cribbage.clj
+++ cribbage.clj
@@ -1,7 +1,6 @@
 (defn make-deck []
   (for [suit (seq "CSHD")
         card (map vector
-                (seq "A23456789TJQK") 
                 [1 2 3 4 5 6 7 8 9 10 11 12 13]
                 [1 2 3 4 5 6 7 8 9 10 10 10 10])]
       (into [suit] card)))
@@ -9,17 +8,17 @@
 (defn suit [card]
   (nth card 0))
 
-(defn face [card]
+(defn rank [card]
   (nth card 1))
 
-(defn rank [card]
-  (nth card 2))
-
 (defn value [card]
-  (nth card 3))
+  (nth card 2))
 
 (defn sort-hand [hand]
   (sort-by rank hand))
+
+(defn other-player [player]
+  (if (= player 0) 1 0))
 
 (defn remove-from-hand [hand card-index]
   (concat (subvec hand 0 card-index)
@@ -34,13 +33,21 @@
         new-scores (assoc (game :scores) player new-score)]
     (assoc game :scores new-scores)))
 
-(defn score-15 [cards]
-  (if (= (sum-values cards) 15)
-    2 0))
+(defn same-rank-length [cards]
+  (loop [first-card (first cards)
+         remainder (rest cards)
+         acc 1]
+    (if (and (not (empty? remainder))
+             (= (rank first-card) (rank (first remainder))))
+      (recur (first remainder) (rest remainder) (+ acc 1))
+      acc)))
 
-(defn score-31 [cards]
-  (if (= (sum-values cards) 31)
-    2 0))
+(defn score-pairs [cards]
+  (condp = (same-rank-length cards)
+    2 2
+    3 6
+    4 12
+    0))
 
 (defn is-run? [cards]
   (let [sorted (sort-hand cards)]
@@ -54,11 +61,19 @@
             false))))))
 
 (defn score-run [cards]
-  (if (< (count cards) 3)
-    0
-    (if (is-run? cards)
-      (count cards)
-      (recur (rest cards)))))
+  (cond (< (count cards) 3) 0
+        (is-run? cards) (count cards)
+        :else (recur (rest cards))))
+
+(defn can-play? [game player]
+  (let [remaining (- 31 (sum-values (game :play-cards)))
+        played-cards (nth (game :played-cards) player)]
+    (loop [hand (nth (game :played-cards) player)
+           index 0]
+      (cond (empty? hand) false
+            (and (not (contains? played-cards index))
+                 (<= (value (first (hand))) remaining)) true
+            :else (recur (rest hand) (+ index 1))))))
 
 (defn add-to-crib [game player card-index]
   (let [hand (nth (game :hands) player)]
@@ -70,6 +85,16 @@
         (assoc game :hands new-hands :crib crib))
       game)))
 
+(defn score-play [game player]
+  (let [play-cards (game :play-cards)
+        values (sum-values play-cards)
+        can-play (can-play? (other-player player))]
+    (+ (score-run play-cards)
+       (score-pairs play-cards)
+       (if (= values 15) 2 0)
+       (if (= values 31) 1 0)
+       (if (not can-play) 1 0))))
+       
 (defn play-card [game player card-index]
   (let [played-cards (nth (game :played-cards) player)]
     (if (not (contains? played-cards card-index))
@@ -89,7 +114,7 @@
               (sort-hand (subvec deck 6 12))]
       :played-cards [#{}, #{}]
       :scores [0 0]
-      :play-cards []
+      :play-cards '()
       :crib []
       :starter (nth deck 12) } ))
 
